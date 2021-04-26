b643d1cf274ac75a51583c2e4a009a239a7db47d
PhysiqueLoper <jjhw0106@naver.com>
2021-04-26 20:13:40 +0900

    알고리즘 프로젝트 생성

---
+++ Algorithm/src/algorithm/Permutation.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+public class Permutation {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	}
+
+	public static void swap() {
+
+	}
+
+	public static boolean check() {
+		boolean check = false;
+		
+		return check;
+	}
+
+}
