84bfcda0e0e30893948bcf8b43f467209697107b
PhysiqueLoper <jjhw0106@naver.com>
2021-04-29 13:03:55 +0900

    .

--- Algorithm/src/algorithm/Permutation.java
+++ Algorithm/src/algorithm/Permutation.java
@@ -1,35 +1,29 @@
 package algorithm;
 
+import java.awt.print.Printable;
 import java.util.ArrayList;
 
 public class Permutation {
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3 };
-		permutation(arr);
+		int[] arr = { 1, 2, 3, 4 };
+		permutation(arr, 0, arr.length, arr.length);
 	}
 
-	public static int[] swap(int[] arr, int a, int b) {
+	public static void swap(int[] arr, int a, int b) {
 		int temp = arr[a];
 		arr[a] = arr[b];
 		arr[b] = temp;
-		return arr;
 	}
 
-	public static void permutation(int[] arr) {
-		if (arr.length == 2) {
-			swap(arr, 0, 1);
-			for (int i : arr) {
-				System.out.println(i);
-			}
+	public static void permutation(int[] arr, int depth, int n, int r) {
+		if (depth == r) {
+			System.out.print(arr[r]);
 			return;
 		}
-		int[] temp = null;
-		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				temp = swap(arr, i, j);
-			}
-		}
-		permutation(temp);
+		for(int i=depth; i<n; i++)
+		swap(arr,depth,i );
+		permutation()
+
 	}
 
 }
