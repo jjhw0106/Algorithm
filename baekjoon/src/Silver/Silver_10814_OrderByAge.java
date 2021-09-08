fdbedac62b4bf8745ed774418b80f79c47566bad
PhysiqueLoper <jjhw0106@naver.com>
2021-09-09 00:56:02 +0900

    UPDATE: 알고리즘

--- baekjoon/src/Silver/Silver_10814_OrderByAge.java
+++ baekjoon/src/Silver/Silver_10814_OrderByAge.java
@@ -4,30 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Person{
+	int age;
+	String name;
+}
 
 public class Silver_10814_OrderByAge {
-	static class Name {
-		int age;
-		String name;
-	}
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		ArrayList<Name> list = new ArrayList<>();
-
-		String name = "";
-		for (int i = 0; i < n; i++) {
-			Name temp = new Name();
-			temp.age = Integer.parseInt(br.readLine());
-			temp.name = br.readLine();
-			list.add(temp);
+		
+		List<Person> list = new ArrayList<Person>();
+		
+		for(int i=0; i<n; i++) {
+			String[] arr=br.readLine().split(" ");
+			Person person = new Person();
+			person.age = Integer.parseInt(arr[0]);
+			person.name = arr[1];
+			list.add(person);
 		}
-
-	}
-
-	static public void solution() {
-
+		
+		Collections.sort(list, Comparator<Person>);
 	}
 }
