705eb9ad00a247d75eba69c8b9159030d33c61f6
PhysiqueLoper <jjhw0106@naver.com>
2021-09-14 02:19:23 +0900

    UPDATE

--- baekjoon/src/Silver/Silver_1065.java
+++ baekjoon/src/Silver/Silver_1065.java
@@ -13,7 +13,7 @@ public class Silver_1065 {
 			if (gap != a) {
 				return true;
 			}
-			if (num == 0) {
+			if (num < 10) {
 				break;
 			}
 		}
@@ -23,12 +23,14 @@ public class Silver_1065 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		boolean check[] = new boolean[n];
-		for (int i = 1; i < n; i++) {
+		boolean check[] = new boolean[n+1];
+		int answer = 0;
+		for (int i = 1; i <= n; i++) {
 			check[i] = solution(i);
 			if(check[i]==false) {
-				System.out.println(i);
+				answer++;
 			}
 		}
+		System.out.println(answer);
 	}
 }
