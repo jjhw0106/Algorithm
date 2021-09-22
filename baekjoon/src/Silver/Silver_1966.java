710db9319029a0d2aa5c177013b24cf6c1440d92
PhysiqueLoper <jjhw0106@naver.com>
2021-09-22 19:02:58 +0900

    UPDATE: 1966번 문제 풀이

--- baekjoon/src/Silver/Silver_1966.java
+++ baekjoon/src/Silver/Silver_1966.java
@@ -3,6 +3,9 @@ package Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -10,34 +13,55 @@ import java.util.StringTokenizer;
 public class Silver_1966 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int no = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		boolean isMax = false;
-		int max = 0;
-		while (no-- > 0) {
-			String[] str = br.readLine().split(" ");
-			int n = Integer.parseInt(str[0]);
-			int target = Integer.parseInt(str[1]);
-			int[] prt = new int[n];
 
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			Queue<Integer> q = new LinkedList<Integer>();
+		// 1. 테스트케이스의 갯수
+		int t = Integer.parseInt(br.readLine());
+		int idx = 0;
+		while (t-- > 0) {
+			// 2. 문서 갯수 n, 궁금한 문서 m
+			String str = br.readLine();
+			int n = Integer.parseInt(str.split(" ")[0]);
+			int m = Integer.parseInt(str.split(" ")[1]);
+
+			// 3. 각 문서 중요도
+			LinkedList<int[]> q = new LinkedList<>();
+
+			String[] arr = br.readLine().split(" ");
+			int[] print = new int[2];
 			for (int i = 0; i < n; i++) {
-				prt[i] = Integer.parseInt(st.nextToken());
-				if (prt[i] > max) {
-					max = prt[i];
-				}
-				q.add(prt[i]);
+				print[0] = i; // idx
+				print[1] = Integer.parseInt(arr[i]); // 중요도
+				q.offer(print);
 			}
-			for (Integer integer : q) {
-				if(integer == max) {
-					q.poll();
-				}else {
-					q.offer(q.poll());
-				}
-				
-			}
+//			max인지 판단
+//			max가 아니면 큐의 맨 뒤로
 
+			int cnt = 0;
+			boolean isMax = true;
+			while (!q.isEmpty()) {
+				int[] target = q.poll();
+				cnt++;
+				for (int i = 0; i < q.size(); i++) {
+					if (target[1] < q.get(i)[1]) {
+						q.offer(target);
+						isMax = false;
+						break;
+					}
+				}
+				if (isMax == true && target[0] == m) {
+					sb.append(cnt);
+				}
+				isMax = false;
+			}
+//			max면 타겟 idx인지 확인
+//			아니면 다음 loop
 		}
+		System.out.println(sb);
 	}
+
+//	static int findMax(int[] arr) {
+//		Arrays.sort(arr,com);
+//		return arr[arr.length - 1];
+//	}
 }
