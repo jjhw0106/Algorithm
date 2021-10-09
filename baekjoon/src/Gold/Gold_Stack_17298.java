903f6d7a3ad23d2f81114a474a9c4009de8c4ebf
PhysiqueLoper <jjhw0106@naver.com>
2021-10-09 23:13:39 +0900

    UPDATE : 스택 오큰수

--- baekjoon/src/Gold/Gold_Stack_17298.java
+++ baekjoon/src/Gold/Gold_Stack_17298.java
@@ -3,66 +3,47 @@ package Gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Gold_Stack_17298 {
+	static Stack<Integer> stack;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Scanner sc = new Scanner(System.in);
 
-		int num = sc.nextInt();
-		int[] attr = new int[num];
-		for (int i = 0; i < num; i++) {
-			attr[i] = sc.nextInt();
-		}
+		int n = Integer.parseInt(br.readLine());
+		String str = br.readLine();
 
-		Stack<Integer> stack = new Stack<Integer>();
-		for (int i = 0; i < attr.length; i++) {
-			stack.push(attr[i]);
+		StringTokenizer st = new StringTokenizer(str, " ");
+
+		int[] arr = new int[n];
+		int[] target = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			target[i] = arr[i];
 		}
-		Stack<Integer> aStack = new Stack<Integer>();
-		aStack.push(-1);
-		int a = 0;
-		int temp = 0;
-		for (int i = 1; i < num; i++) {
-			if ((temp = stack.pop()) > stack.peek()) {
-				a = temp;
-				aStack.push(a);
-			} else {
-				if (a > stack.peek()) {
-					aStack.push(a);
-				} else {
-					aStack.push(-1);
-				}
-			}
+		Arrays.sort(arr);
+
+		List<Integer> ansList = new ArrayList<Integer>();
+		// arr 정렬해서 stack에 저장
+		stack = new Stack<Integer>();
+		for (int i = arr.length - 1; i >= 0; i--) {
+			stack.push(arr[i]);
 		}
-		StringBuilder sb = new StringBuilder();
-		while (!aStack.isEmpty()) {
-			sb.append(aStack.pop());
-			if (aStack.size() > 0) {
-				sb.append(" ");
-			}
+		
+		int idx = 0;
+		
+		if(target[idx] < stack.pop()) {
+			
 		}
-		System.out.println(sb);
-//		시간초과
-//		StringBuilder sb = new StringBuilder();
-//		for (int i = 0; i < num; i++) {
-//			if (i == num - 1) {
-//				sb.append(-1);
-//				break;
-//			}
-//			for (int j = i; j < num; j++) {
-//				if (attr[i] < attr[j]) {
-//					sb.append(attr[j]).append(" ");
-//					break;
-//				}
-//				if (j == num - 1) {
-//					sb.append(-1).append(" ");
-//					break;
-//				}
-//			}
-//		}
-//		System.out.println(sb);
+		
+		
+		for (Integer integer : ansList) {
+			System.out.println(integer);
+		}
 	}
 }
