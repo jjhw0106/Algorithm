e0d78cf6d718afa6d9b921f663278324b03a1a1b
PhysiqueLoper <jjhw0106@naver.com>
2021-10-24 23:30:31 +0900

    update

---
+++ baekjoon/src/Silver/Silver_greedy_1931.java
@@ -0,0 +1,20 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Silver_greedy_1931 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		String time = "";
+		int cnt = 0;
+		// 하나씩 다 돌면서 cnt++ 하고 max값 구해서 반환
+		// 끝나는 시간 기준으로 정렬
+		// i+1번의 시작 시간이 i번째 끝나는시간보다 작으면 pass 
+	}
+}
