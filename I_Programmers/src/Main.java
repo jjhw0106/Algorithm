44c8ae2be61f25ffd2c930958bf479ea7ded61dd
DESKTOP-QUPDGLB\jjhw0 <jjhw0106@naver.com>
2022-06-30 15:13:25 +0900

    UPDATE : 인텔리제이 알고리즘 풀이

--- I_Programmers/src/Main.java
+++ I_Programmers/src/Main.java
@@ -1,65 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Array;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi", "apeach muzi"};
-        sol.solution(id_list, report, 2);
-//        sol.checkReport(report,2);
-//        Map<String, ArrayList<String>> map = new HashMap<>();
-//        map.put("A",new ArrayList<>());
-//        map.put("B",new ArrayList<>());
-//        map.put("C",new ArrayList<>());
-//        map.put("D",new ArrayList<>());
-//        map.get("A").add("B");
-//        map.get("B").add("F");
-//        map.get("C").add("D");
-//        map.get("D").add("F");
-//        map.get("A").add("A");
-//        map.get("A").add("A");
-//
-//        Map<String, HashSet<String>> map2 = new HashMap<>();
-//        map2.put("A",new HashSet<String>());
-//        map2.put("C",new HashSet<String>());
-//        map2.put("B",new HashSet<String>());
-//        map2.put("D",new HashSet<String>());
-//        map2.get("A").add("B");
-//        map2.get("B").add("F");
-//        map2.get("C").add("D");
-//        map2.get("D").add("F");
-//        map2.get("A").add("A");
-//        map2.get("A").add("A");
-//
-//        Map<String, String> map3 = new HashMap<>();
-//        map3.put("A", "B");
-//        map3.put("B", "F");
-//        map3.put("C", "D");
-//        map3.put("D", "F");
-//        map3.put("A", "A");
-//
-//        System.out.println(map.toString());
-    }
-}
+    public static void main(String[] args) throws IOException {
 
-class Solution {
-    public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ask = new StringBuilder();
+        StringBuilder pre = new StringBuilder();
+        System.out.println(recurAsk(ask,3));
 
-        Map<String, HashSet<String>> reportMap = new HashMap<>();
-        Map<String, Integer> mailMap;
-
-
-
-        // [신고 당한애 : 신고당한 횟수]
-        // [신고 한애 : 답장 수]
-        //
-        return answer;
-    }
-
-    public ArrayList<String> checkReport(String[] report, int k){
 
     }
+    static StringBuilder recurAsk(StringBuilder ask, int num){
+        if(num == 1){
+            ask.append("재귀함수가 뭔가요?\n");
+            ask.append("잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+            ask.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+            ask.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\n");
+            return ask;
+        }
 
+        return ask.append(recurAsk(ask.insert(0,"----"), num-1));
+    }
+    static StringBuilder recurAnswer(StringBuilder ask, int num){
+
+        StringBuilder ans = new StringBuilder();
+        if(num == 1){
+            return ask;
+        }
+
+        return ans.append("----").append(ask);
+    }
 }
---
+++ I_Programmers/src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+    String u = "";
+    String v = "";
+    public String solution(String s) {
+
+        return "";
+    }
+}
